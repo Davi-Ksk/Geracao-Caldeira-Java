a3387e0b017b332089bd35b9b1f6b38657bc1bb7
Davi Kraieski <davikraieski@gmail.com>
2023-09-05 16:47:13 -0300

    first commit

---
+++ Aula01/Exercicio01/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+
+        String userName = myObj.nextLine();
+        System.out.println("Seu nome é: " + userName);
+    }
+}

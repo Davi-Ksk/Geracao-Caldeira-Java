3ee627b3b1a4b316f8a3fef50af48e27b701f3d1
davi <davikraieski@gmail.com>
2023-11-05 18:36:10 -0300

    feat: exercicio06 finished

--- Aula05/exercicio06/src/main/java/Menu.java
+++ Aula05/exercicio06/src/main/java/Menu.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
+
 public class Menu {
+    public static void menu(){
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        String name = "";
+        String local = "";
+        String cpf = "";
+        Reservations reservations = new Reservations(name, local, cpf);
+
+
+        do {
+            System.out.println("----------------------Menu-----------------------");
+            System.out.println("1 - Reservar uma passagem");
+            System.out.println("2 - Cancelar reserva");
+            System.out.println("3 - Listar minhas reservas");
+            System.out.println("4 - Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> {
+                    reservations.addReservation();
+                }
+
+                case 2 -> {
+                    reservations.cancelReservation();
+                }
+
+                case 3 -> {
+                    reservations.listReservations();
+                }
+
+                case 4 -> {
+                    System.out.println("Saindo...");
+                    exit = true;
+                }
+
+                default -> System.out.println("Opção invalida!");
+            }
+
+        } while (!exit);
+
+    }
+
 }
--- Aula05/exercicio06/src/main/java/Reservations.java
+++ Aula05/exercicio06/src/main/java/Reservations.java
@@ -1,3 +1,56 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Reservations {
+
+    String name;
+    String local;
+    String cpf;
+
+    LinkedList<String> reservations = new LinkedList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public Reservations(String name, String local, String cpf) {
+        this.name = name;
+        this.local = local;
+        this.cpf = cpf;
+    }
+
+    public void addReservation(){
+
+        System.out.println("Digite seu nome: ");
+        this.name = scanner.nextLine();
+
+        System.out.println("Digite seu cpf: ");
+        this.cpf = scanner.nextLine();
+
+        System.out.println("Digite o local: ");
+        this.local = scanner.nextLine();
+
+        String reservation = name + ", " + cpf + ", " + local;
+        reservations.add(reservation);
+
+        System.out.println("Reserva realizada com sucesso!");
+    }
+
+    public void cancelReservation(){
+
+        System.out.println("Digite o número da reserva que deseja cancelar: ");
+        int reservationNumber = scanner.nextInt();
+
+        reservations.remove(reservationNumber - 1);
+
+        System.out.println("Reserva cancelada com sucesso!");
+    }
+
+    public void listReservations(){
+
+        int count = 1;
+
+        for (String reservation : reservations) {
+            System.out.println(count + ": " + reservation);
+            count ++;
+        }
+    }
 
 }

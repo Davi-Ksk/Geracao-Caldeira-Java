65ea2a85565290d63654fe8973c561b1b216b6a3
davi <davikraieski@gmail.com>
2023-11-08 10:11:19 -0300

    add: People Class

---
+++ Aula06/exercicio02/src/main/java/People.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class People {
+
+    private String name;
+    private int age;
+
+    Map<String, Integer> peopleMap = new HashMap<String, Integer>();
+
+    public People(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void addPerson(){
+
+    }
+
+    public void removePerson(){
+
+    }
+
+    public void showPeople(){
+
+    }
+
+    public void showPeopleAge(){
+
+    }
+
+    public void showElderPeople(){
+
+    }
+
+}

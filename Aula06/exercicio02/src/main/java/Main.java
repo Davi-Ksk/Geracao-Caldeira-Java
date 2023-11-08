41cecadaf2fe303b378b73b33711caf3b7c62edd
davi <davikraieski@gmail.com>
2023-11-08 10:10:15 -0300

    add: main

---
+++ Aula06/exercicio02/src/main/java/Main.java
@@ -0,0 +1,11 @@
+// Exercício 2: HashMap com pessoas
+// Crie um programa Java que utilize uma estrutura de dados do tipo HashMap para associar nomes a idades.
+// Insira algumas entradas no mapa e, em seguida, acesse e imprima a idade de um nome específico
+// Crie um método para imprimir nomes das pessoas que estão na terceira idade.
+// Certifique-se de lidar com o caso em que o nome não está presente no mapa.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
